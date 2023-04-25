f20a6235c7f71957e4676193ecb2e25bc201bb3e
GuglielmoBini <guglielmobini@gmail.com>
2023-04-25 16:32:48 +0200

    corrections

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,20 +6,23 @@
         {{ __('Dashboard') }}
     </h2>
     <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+        <div class="col-12">
+            <div class="card text-center">
+                <h3 class="card-header">{{ __('Dashboard Utente') }}</h3>
 
-                <div class="card-body">
+                <div class="card-body fs-3 text-success p-5">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Hai effettuato il Login con successo!') }}
                 </div>
             </div>
+        </div>
+        <div class="col-3 text-center mt-5">
+            <a class="btn btn-primary" href="{{ route('admin.companies.index') }}">{{ __('VAI ALLE AZIENDE') }}</a>
         </div>
     </div>
 </div>
